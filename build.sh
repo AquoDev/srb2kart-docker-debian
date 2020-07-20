18d311bbd20a9e42688d9143f1e613beebbad3e9
AquoDev <14019792+AquoDev@users.noreply.github.com>
2020-07-20 10:54:16 +0200

    Fixed shellcheck warnings

--- build.sh
+++ build.sh
@@ -16,11 +16,11 @@ if [ -z "$VERSION" ]; then
 fi
 
 # Make useful values to handle the process
-NO_DOT_VERSION=`echo $VERSION | sed 's/[.]//g'`
+NO_DOT_VERSION=$(echo "$VERSION" | sed 's/[.]//g')
 NAME=srb2kart-docker-debian
 IMAGE=$NAME:$VERSION
 
 # Build the game and copy from the container release files and assets to host (./build directory)
-docker image build --rm --tag $IMAGE --build-arg VERSION=$VERSION --build-arg NO_DOT_VERSION=$NO_DOT_VERSION .
-docker run --rm --name $NAME -d $IMAGE
+docker image build --rm --tag "$IMAGE" --build-arg VERSION="$VERSION" --build-arg NO_DOT_VERSION="$NO_DOT_VERSION" .
+docker run --rm --name $NAME -d "$IMAGE"
 docker cp $NAME:/build .
